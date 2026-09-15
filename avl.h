80db0cdd1afc2b89d76952a36a59b24c93181a46
Ali Yasin G <aygmedia7@gmail.com>
2026-09-15 20:38:54 +0300

    Update avl.h

--- avl.h
+++ avl.h
@@ -1,7 +1,7 @@
 /*
  * Vanitygen, vanity bitcoin address generator
  * Copyright (C) 2011 <samr7@cs.washington.edu>
- *
+ * 
  * Vanitygen is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
